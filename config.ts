23c11a1985c3cf642e17c9eafbba17a9fdd11894
Innei <i@innei.in>
2023-09-06 19:25:50 +0800

    update config

--- config.ts
+++ config.ts
@@ -1,37 +1,38 @@
 export const opensource = {
   active: [
-    'Innei/Shiro', 
-    'mx-space/kami',
-    'mx-space/core',
-    'mx-space/mx-admin',
-    
-    'Innei/bump-version',
+    "Innei/Shiro",
+    "mx-space/kami",
+    "mx-space/core",
+    "mx-space/mx-admin",
+
+    // 'Innei/bump-version',
+    "Innei/nvim-config-lua",
   ],
   toys: {
     limit: 3,
     random: true,
     repos: [
-      'reactify-component/react-typewriter',
-      'reactify-component/react-message',
-      'Innei/react-shortcut-guide',
-      'Innei/simple-clock',
-      'Innei/shuffle-article',
-      'Innei/candy',
-      'Innei/pageproxy',
-      'Innei/animate-uri',
-      'Innei/vue-react-wrapper',
-      'Innei/next-async'
+      "reactify-component/react-typewriter",
+      "reactify-component/react-message",
+      "Innei/react-shortcut-guide",
+      "Innei/simple-clock",
+      "Innei/shuffle-article",
+      "Innei/candy",
+      "Innei/pageproxy",
+      "Innei/animate-uri",
+      "Innei/vue-react-wrapper",
+      "Innei/next-async",
     ],
   },
-}
-export const motto = `<p align=center><strong>お可愛いこと。</strong></p>`
+};
+export const motto = `<p align=center><strong>お可愛いこと。</strong></p>`;
 export const github = {
-  name: 'Innei',
-}
+  name: "Innei",
+};
 
 export const mxSpace = {
-  url: 'https://innei.in',
-  api: 'https://ingress.innei.in/api/v2',
-}
+  url: "https://innei.in",
+  api: "https://ingress.innei.in/api/v2",
+};
 
-export const timeZone = 'Asia/Shanghai'
+export const timeZone = "Asia/Shanghai";
