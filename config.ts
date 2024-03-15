54e5cc8fcf8ef868aaecc6ce55f140317545dbf8
Innei <tukon479@gmail.com>
2024-03-15 16:27:13 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -32,7 +32,7 @@ export const github = {
 
 export const mxSpace = {
   url: "https://innei.in",
-  api: "https://api.innei.ren/api/v2",
+  api: "https://api.innei.ren/v2",
 };
 
 export const timeZone = "Asia/Shanghai";
