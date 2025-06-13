0dc996f8d855e4c5255b4bcc75ed8a4cacc71019
Innei <tukon479@gmail.com>
2025-06-13 16:29:55 +0800

    Refactor config.ts for consistency in string formatting and structure
    Signed-off-by: Innei <tukon479@gmail.com>

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 export const opensource = {
   active: [
-    "Innei/Shiro",
-   // "mx-space/kami",
-    "mx-space/core",
-    "RSSNext/follow",
-    'Innei/book-ssg-template'
+    'Innei/Shiro',
+    // "mx-space/kami",
+    'mx-space/core',
+    'RSSNext/follow',
+    'Innei/book-ssg-template',
 
     // 'Innei/bump-version',
     // "Innei/nvim-config-lua",
@@ -13,27 +13,27 @@ export const opensource = {
     limit: 3,
     random: true,
     repos: [
-      "reactify-component/react-typewriter",
-      "reactify-component/react-message",
-      "Innei/react-shortcut-guide",
-      "Innei/simple-clock",
-      "Innei/shuffle-article",
-      "Innei/candy",
-      "Innei/pageproxy",
-      "Innei/animate-uri",
-      "Innei/vue-react-wrapper",
-      "Innei/next-async",
+      'reactify-component/react-typewriter',
+      'reactify-component/react-message',
+      'Innei/react-shortcut-guide',
+      'Innei/simple-clock',
+      'Innei/shuffle-article',
+      'Innei/candy',
+      'Innei/pageproxy',
+      'Innei/animate-uri',
+      'Innei/vue-react-wrapper',
+      'Innei/next-async',
     ],
   },
-};
-export const motto = `<p align=center><strong>お可愛いこと。</strong></p>`;
+}
+export const motto = `<strong>お可愛いこと。</strong>`
 export const github = {
-  name: "Innei",
-};
+  name: 'Innei',
+}
 
 export const mxSpace = {
-  url: "https://innei.in",
-  api: "https://api.innei.ren/v2",
-};
+  url: 'https://innei.in',
+  api: 'https://api.innei.ren/v2',
+}
 
-export const timeZone = "Asia/Shanghai";
+export const timeZone = 'Asia/Shanghai'
